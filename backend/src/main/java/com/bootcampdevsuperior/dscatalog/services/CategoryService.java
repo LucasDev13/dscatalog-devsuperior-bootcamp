91bde6a68162c511b000f5492703044eab3ed0db
Lucas <lucaspontes140@gmail.com>
2022-02-21 10:39:24 -0300

    FindById.

--- backend/src/main/java/com/bootcampdevsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bootcampdevsuperior/dscatalog/services/CategoryService.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private Category entity;
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
@@ -22,5 +24,12 @@ public class CategoryService {
     public List<CategoryDto> findAll(){
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(CategoryDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDto findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDto(entity);
     }
 }
